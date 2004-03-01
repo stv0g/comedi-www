b9e8c455cdc53b9c5ebc7d1333ef2b58e84a6037
David Schleef <ds@schleef.org>
2004-03-01 17:38:43 +0000

    fix link

--- links.php
+++ links.php
@@ -14,7 +14,7 @@
 <li><a href="http://www.datx.com">Data Translation</a>
 <li><a href="http://www.instrument.com/">Intelligent Instrumentation</a>
 <li><a href="http://www.iotech.com/">IOtech</a>
-<li><a href="http://www.www.linux-usb-daq.com.com/">Incite Technology Limited</a>
+<li><a href="http://www.linux-usb-daq.com/">Incite Technology Limited</a>
 <li><a href="http://www.keithley.com/">Keithley Metrabyte</a>
 <li><a href="http://www.natinst.com">National Instruments</a>
 <li><a href="http://www.quanser.com">Quanser Consulting</a>
