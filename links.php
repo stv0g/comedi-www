1fcfe02685bb159bb9baaba2ffb896d0a5ec6b0f
David Schleef <ds@schleef.org>
2004-04-19 20:30:34 +0000

    add quatech link

--- links.php
+++ links.php
@@ -10,14 +10,15 @@
 <li><a href="http://www.advantech.com">Advantech</a>
 <li><a href="http://www.adlink.com.tw">ADLink</a>
 <li><a href="http://www.analog.com/industry/ios/index.html">Analog Devices</a>
-<li><a href="http://www.computerboards.com">ComputerBoards</a>
 <li><a href="http://www.datx.com">Data Translation</a>
 <li><a href="http://www.instrument.com/">Intelligent Instrumentation</a>
 <li><a href="http://www.iotech.com/">IOtech</a>
 <li><a href="http://www.linux-usb-daq.com/">Incite Technology Limited</a>
 <li><a href="http://www.keithley.com/">Keithley Metrabyte</a>
+<li><a href="http://www.measurementcomputing.com">Measurement Computing</a> (formerly ComputerBoards)
 <li><a href="http://www.natinst.com">National Instruments</a>
 <li><a href="http://www.quanser.com">Quanser Consulting</a>
+<li><a href="http://www.superlogics.com">SuperLogics</a> (formerly Quatech)
 </ul>
 
 <?php
